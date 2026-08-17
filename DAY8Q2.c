82bb6feeb09c02162157b99d3f397db8b08a8c44
nikitasinghdeswal <deswalnikitasingh@gmail.com>
2026-08-17 07:31:17 +0530

    Add program to find the largest of three numbers
    Implement a program to find the largest of three numbers using if-else statements.

---
+++ DAY8Q2.c
@@ -0,0 +1,44 @@
+//Q16: Write a program to input three numbers and find the largest among them using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+3 7 5
+Output 1:
+Largest is 7
+
+Input 2:
+-1 -5 0
+Output 2:
+Largest is 0
+
+*/
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+int c; 
+scanf("%d %d %d", &a, &b, &c);
+if ( a > b )
+{
+    if ( a> c)
+    {
+        printf(" %d is the largest number", a);
+    }
+    else
+    {
+        printf(" %d is the largest number", c);
+    }
+}
+if ( b> c)
+{
+    printf("%d is the largest number", b);
+    }
+    else 
+    {
+        printf("%d is the largest number", c);
+    }
+    
+return 0;
+}
