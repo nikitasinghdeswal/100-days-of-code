7e7eead10ad828d3a78ab50883dc0ded3db43477
nikitasinghdeswal <deswalnikitasingh@gmail.com>
2026-08-18 07:45:03 +0530

    Implement grade assignment based on percentage input
    Refactor grading logic and clean up comments.

---
+++ DAY9Q2.c
@@ -0,0 +1,65 @@
+//Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+//90-100: Grade A 
+//80-89: Grade B 
+//70-79: Grade C 
+//60-69: Grade D 
+//below 60: Grade F.
+
+/*
+Sample Test Cases:
+Input 1:
+95
+Output 1:
+Grade A
+
+Input 2:
+82
+Output 2:
+Grade B
+
+Input 3:
+68
+Output 3:
+Grade D
+
+Input 4:
+50
+Output 4:
+Grade F
+
+*/
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if ( a>= 90 && a<=100)
+    {
+        printf("Grade A");
+
+    }
+if ( a>= 80 && a<=89)
+{ 
+    printf("Grade B");
+}
+    if ( a>= 70 && a<=79)
+{
+    printf("Grade C");
+}
+{
+    if ( a>= 60 && a<=69)
+    {
+        printf("Grade D");
+        
+    }
+{
+    if (a<60)
+    {
+        printf("Grade F");
+
+    }
+}
+
+}
+return 0;
+}
