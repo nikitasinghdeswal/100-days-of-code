0c354ce19ae91934c45257c8c3b8a2d240f9c390
nikitasinghdeswal <deswalnikitasingh@gmail.com>
2026-08-28 21:45:18 +0530

    Implement LCM calculation for two numbers
    This program calculates the LCM of two input numbers by finding the smallest common multiple.

---
+++ DAY19Q1.c
@@ -0,0 +1,45 @@
+//Q37: Write a program to find the LCM of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+4 5
+Output 1:
+20
+
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+#include <stdio.h>
+int main() 
+{
+    int a; 
+    int b;
+    int max; 
+    scanf("%d %d", &a, &b);
+
+    
+    if (a > b) 
+    {
+        max = a;
+    }
+     else 
+     {
+        max = b;
+    }
+    while (1)
+     {
+        if (max % a == 0 && max % b == 0)
+         {
+            
+            printf("%d\n", max);
+            break;
+        }
+        max = max + 1;
+    }
+
+    return 0;
+}
