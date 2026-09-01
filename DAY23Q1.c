d93a6a029811b186d14f659c3af7f5f4e04f8efb
nikitasinghdeswal <deswalnikitasingh@gmail.com>
2026-09-01 20:29:34 +0530

    Implement pattern printing program in C

---
+++ DAY23Q1.c
@@ -0,0 +1,36 @@
+/*Q46: Write a program to print the following pattern:
+*****
+*****
+*****
+*****
+*****
+
+sample Test Cases:
+Input 1:
+
+Output 1:
+*****
+*****
+*****
+*****
+*****
+
+*/
+#include <stdio.h>
+int main() 
+{
+    int rows = 5;
+    int columns = 5;
+    for (int i = 0; i < rows; i++) 
+    {
+        
+        for (int j = 0; j < columns; j++)
+         {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
