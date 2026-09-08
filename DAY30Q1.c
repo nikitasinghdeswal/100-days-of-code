aa729d090025f65357c3394cf9718f5efeebcd06
nikitasinghdeswal <deswalnikitasingh@gmail.com>
2026-09-08 22:51:41 +0530

    Implement even and odd number counter in array
    This program counts the number of even and odd integers in an array based on user input.

---
+++ DAY30Q1.c
@@ -0,0 +1,42 @@
+/*Q59: Count even and odd numbers in an array.
+
+/*
+Sample Test Cases:
+Input 1:
+6
+1 2 3 4 5 6
+Output 1:
+Even=3, Odd=3
+
+Input 2:
+4
+2 4 6 8
+Output 2:
+Even=4, Odd=0
+
+*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 0;
+    }
+
+    int e = 0;
+    int o = 0;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        if (x % 2 == 0) {
+            e++;
+        } else {
+            o++;
+        }
+    }
+
+    printf("Even=%d, Odd=%d\n", e, o);
+
+    return 0;
+}
