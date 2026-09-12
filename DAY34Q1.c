008bf8fbfc92b72c6f9ea770480596e95457debf
nikitasinghdeswal <deswalnikitasingh@gmail.com>
2026-09-12 19:52:42 +0530

    Add element insertion in array at given position
    Implement functionality to insert an element into an array at a specified position.

---
+++ DAY34Q1.c
@@ -0,0 +1,45 @@
+/*Q67: Insert an element in an array at a given position.
+
+/*
+Sample Test Cases:
+Input 1:
+4
+10 20 30 40
+2 15
+Output 1:
+10 20 15 30 40
+
+*/
+#include <stdio.h>
+
+int main() 
+{
+    int n, a[1000], p, e, i;
+    
+    scanf("%d", &n);
+    for (i = 0; i < n; i++) 
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    scanf("%d %d", &p, &e);
+    
+    for (i = n; i > p; i--) 
+    {
+        a[i] = a[i - 1];
+    }
+    
+    a[p] = e;
+    n++;
+    
+    for (i = 0; i < n; i++)
+     {
+        printf("%d", a[i]);
+        if (i < n - 1) {
+            printf(" ");
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}
