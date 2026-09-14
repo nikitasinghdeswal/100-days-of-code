a48744484aaed30b8ec0574d41a8ce06e471c081
nikitasinghdeswal <deswalnikitasingh@gmail.com>
2026-09-14 23:35:03 +0530

    Add matrix input and output
    Implement matrix reading and printing functionality.

---
+++ DAY36Q1.c
@@ -0,0 +1,49 @@
+/*Q71: Read and print a matrix.
+
+/*
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+Output 1:
+1 2
+3 4
+
+*/
+#include <stdio.h>
+
+int main() 
+{
+    int rows, cols;
+
+    if (scanf("%d %d", &rows, &cols) != 2)
+     {
+        return 1;
+    }
+
+    int matrix[rows][cols];
+
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++)
+         {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++)
+     {
+        for (int j = 0; j < cols; j++)
+         {
+            printf("%d", matrix[i][j]);
+            if (j < cols - 1)
+             {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
